58be1efb98690e36150d4307d290756a01e7762b
Gal shaharabani <galshar1@gmail.com>
2019-12-06 11:40:32 +0200

    commit to forked repo

--- site.js
+++ site.js
@@ -1,4 +1,9 @@
 function changeText(){
     document.getElementById("p1").innerHTML="text changed";
     //
+    var a=15;
+    if(a>10){
+        console.log("a bigger then 10");
+        
+    }
 }
